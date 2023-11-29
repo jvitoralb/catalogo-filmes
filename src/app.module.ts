f2c6035483dc3132ebfafaf50915c8c31277fccb
jvitoralb <jvvalb@gmail.com>
2023-11-29 12:40:34 -0300

    update: implement user and auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
-import { MovieModule } from './movies/movie.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './users/user.module';
+import { MovieModule } from './movies/movie.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
-        MovieModule
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        DatabaseModule,
+        MovieModule,
+        AuthModule,
+        UserModule
     ],
     controllers: [],
-    providers: [],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: AuthGuard,
+        }
+    ],
 })
 export class AppModule { }
